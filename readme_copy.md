f1b48441e8909c11b21480d499914069c0d0f515
mohamed bennekrouf <mohamed.bennekrouf@gmail.com>
2013-12-22 19:35:10 +0100

    redis

--- readme_copy.md
+++
@@ -1,9 +0,0 @@
-
-git config --global http.proxy fr-proxy.groupinfra.com:3128
-git config --global https.proxy fr-proxy.groupinfra.com:3128
-
-export http_proxy=http://fr-proxy.groupinfra.com:3128
-export https_proxy=http://fr-proxy.groupinfra.com:3128
-
-npm config set proxy http://fr-proxy.groupinfra.com:3128
-npm config set https-proxy http://fr-proxy.groupinfra.com:3128
--- redis.js
+++ redis.js
@@ -15,5 +15,70 @@ client.get("message1", function(err, reply){
 var message = "toto";
 
 client.lpush("messages", message, function(err, reply){
-console.log(reply);
+console.log(reply); // reply with list length
+});i
+
+
+// retrieving form the list :
+client.lrange("messages", 0, -1, function(err, messages){
+console.log(messages); // return all items of the list
 });
+
+
+var storeMessage = function(name, data){
+	var message = JSON.stringify({name: name, data: data});
+	redisClient.lpush("messages", message, function(err, response){
+		redisClient.ltrim("messages", 0, 10); // keep newest 10 items
+	
+	});
+
+}
+
+// output from list
+client.on('join', function(name){
+	redisClient.lrange("messages", 0, -1, function(err, messages){
+		messages = messages.reverse(); //reverse to emit on right order
+		
+		message.forEach(function(message){
+			message = JSON.parse(message);
+			client.emit("messages", message.name + ": " + message.data);
+		
+		});
+	});
+
+})
+
+// add a list of chatters
+
+client.on('join', function(name){
+	// notify all others
+	client.broadcast.emit("add chatter", name);
+	redisClient.sadd("chatters", name);
+});
+
+// Notify chatters
+client.on('join', function(name){
+	client.broadcast.emit('add chatter', name);
+	redisClient.smembers('name', function(err, names){
+		names.forEach(function(name){
+			client.emit('add chatter', name);
+		
+		});
+	
+	});
+// add name to chatters set
+redisClient.sadd("chatters", name);
+
+
+});
+
+// remove a chatter
+client.on('disconnect', function(name){
+	client.get('nickname', function(err, name){
+		client.broadcast.emit("remove chatter", name);
+		redisClient.srem("chatters", name);
+	})
+});
+
+
+
